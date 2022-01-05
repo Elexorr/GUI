aeafa49df642c4ec3857f61dff0353f649eeda76
Trotun Elexorr <ezofagus@gmail.com>
2022-01-05 10:45:39 +0100

    asd

---
+++ GUIgrid.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk()
+root.title("GUIgrid")
+
+xx = root.winfo_screenwidth()
+yy = root.winfo_screenheight()
+
+# frame1 = tk.Frame(master=root, width=xx/2, height=yy/2, bg="yellow")
+window = tk.Canvas(width = xx - 153, height = yy - 150 , bg = "light grey")  #yy*0.9-80
+frame2 = tk.Frame(master=root, width=150, height = yy - 146, bg="grey")   #yy*0.9-76
+frame3 = tk.Frame(master=root, width=xx - 149, height=83, bg="grey")  #yy*0.1
+frame4 = tk.Frame(master=root, width=150, height=83, bg="grey")  #y*0.1
+
+# frame1.grid(row = 0, column = 0, sticky = W, pady = 2)
+window.grid(row = 0, column = 0, sticky = S)
+frame2.grid(row = 0, column = 1, sticky = S)
+frame3.grid(row = 1, column = 0, sticky = E)
+frame4.grid(row = 1, column = 1, sticky = W)
+
+root.mainloop()
